ea7d1e994e6daf60d6914e6df8f5cad601856903
cobeol <cobeol0111@gmail.com>
2022-10-07 13:34:49 +0900

    v1.0.0-alpha.5

--- dist/Wallet/Command/index.d.ts
+++ dist/Wallet/Command/index.d.ts
@@ -1,36 +1,7 @@
-/**
- * 명령
- *
- * @since v1.0.0-alpha
- * @param name 이름
- * @param data 데이터
- */
 export declare class Command {
     name: string;
     data: any;
-    constructor(
-    /**
-     * 이름
-     */
-    name: string, 
-    /**
-     * 데이터
-     */
-    data?: any);
+    constructor(name: string, data?: any);
 }
-/**
- * 데이터가 명령인지 검증합니다
- *
- * @since v1.0.0-alpha
- * @param data
- * @returns boolean
- */
 export declare function isCommandTypeValid(data: any): boolean;
-/**
- * 데이터를 명령으로 변환합니다
- *
- * @since v1.0.0-alpha
- * @param data
- * @returns Command | undefined
- */
 export declare function anyToCommand(data: any): Command | undefined;
--- lib/Wallet/Command/index.ts
+++ lib/Wallet/Command/index.ts
@@ -1,37 +1,15 @@
-/**
- * 명령
- * 
- * @since v1.0.0-alpha
- * @param name 이름
- * @param data 데이터
- */
 export class Command
 {
     public name: string
     public data: any
     
-    constructor(
-        /**
-         * 이름
-         */
-        name: string, 
-        /**
-         * 데이터
-         */
-        data?: any)
+    constructor(name: string, data?: any)
     {
         this.name = name
         this.data = data
     }
 }
 
-/**
- * 데이터가 명령인지 검증합니다
- * 
- * @since v1.0.0-alpha
- * @param data 
- * @returns boolean
- */
 export function isCommandTypeValid(data: any): boolean
 {
     if (data instanceof Object)
@@ -40,13 +18,6 @@ export function isCommandTypeValid(data: any): boolean
     return false
 }
 
-/**
- * 데이터를 명령으로 변환합니다
- * 
- * @since v1.0.0-alpha
- * @param data 
- * @returns Command | undefined
- */
 export function anyToCommand(data: any): Command | undefined
 {
     if (isCommandTypeValid(data))
